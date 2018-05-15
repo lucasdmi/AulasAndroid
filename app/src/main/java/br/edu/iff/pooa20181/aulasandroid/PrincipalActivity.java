a647a6476bd2e56e0b99e9831e8c82aa6266b284
lucasdmi <lucasmisidoro@gmail.com>
2018-05-14 21:36:55 -0300

    AULA01
    Minha primeira tela com o aplicativo Android Studio

--- app/src/main/java/br/edu/iff/pooa20181/aulasandroid/PrincipalActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/aulasandroid/PrincipalActivity.java
@@ -3,13 +3,22 @@ package br.edu.iff.pooa20181.aulasandroid;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class PrincipalActivity extends AppCompatActivity {
+
+    private EditText etNome;
+    private Button btnOK;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +33,20 @@ public class PrincipalActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        etNome = (EditText) findViewById(R.id.edtNome);
+        btnOK = (Button) findViewById(R.id.btOK);
+
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(PrincipalActivity.this);
+                dlg.setMessage(etNome.getText().toString());
+                dlg.setNeutralButton("OK", null);
+                dlg.show();
             }
         });
     }
